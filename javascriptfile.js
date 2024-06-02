9dac76c58eed5cc5db89d9b59be0b3978e312433
Varun Rao <varunvellorerao@gmail.com>
2024-06-02 18:43:46 -0400

    Created React app using Vite and typescript

--- javascriptfile.js
+++ javascriptfile.js
@@ -1,1 +1,7 @@
 console.log("hellohellohello");
+// Vanilla JavaScript
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    const div = document.querySelector('#div');
+    div.style.display = 'none';
+})
