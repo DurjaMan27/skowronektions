e89627fb4099eb039ae5542d2d57fd258851c792
Varun Rao <varunvellorerao@gmail.com>
2024-06-07 14:34:52 -0400

    Creating some basic css classes for where the game will go

--- connections-game/src/App.js
+++ connections-game/src/App.js
@@ -4,6 +4,7 @@ import TestComponent from './components/testComponent';
 
 function App() {
   return (
+    <>
       <div className="App">
         <header className="App-header">
           <h1>This is my game</h1>
@@ -11,6 +12,16 @@ function App() {
           <TestComponent />
         </header>
       </div>
+      <div className="Game-area">
+        <p>This content is below the header</p>
+        <div className="Gameboard">
+          <div><p>This is first block</p></div>
+          <div><p>This is second block</p></div>
+          <div><p>This is third block</p></div>
+          <div><p>This is fourth block</p></div>
+        </div>
+      </div>
+    </>
   );
 }
 
